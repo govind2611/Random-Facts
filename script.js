3a2ed1c27baf575011db27c304ca68cf4ccc41b1
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-24 15:15:22 +0530

    generating random fact
    used an api called api ninja

---
+++ script.js
@@ -0,0 +1,21 @@
+let fact = document.querySelector("#fact");
+let btn = document.querySelector("#generate");
+let apiKey = "3zOMdOjZi57ZQKbJWpSWJA==2yMcQ9n6BHsxSgQv";
+let options = {
+  method: "GET",
+  headers: { "x-api-key": apiKey },
+};
+let url = "https://api.api-ninjas.com/v1/facts?limit=1";
+
+const generateFact = () => {
+  fetch(url, options)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      fact.innerText = data[0].fact;
+    });
+};
+btn.addEventListener("click", generateFact);
+window.addEventListener("load", generateFact);
